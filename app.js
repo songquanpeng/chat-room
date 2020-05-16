3a9f85aca9a7b05fb1d9421960cbf98e46c5824c
Song <quanpengsong@gmail.com>
2020-05-16 22:38:18 +0800

    :tada: file upload api is done

--- app.js
+++ app.js
@@ -1,48 +1,55 @@
-let express = require('express');
-let path = require('path');
-const serveStatic = require('serve-static');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const serveStatic = require("serve-static");
+const uploadRouter = require("./file");
 
 let app = express();
-let server = require('http').createServer(app);
-let io = require('socket.io')(server);
+app.use(cors());
+app.use("/upload", uploadRouter);
+
+let server = require("http").createServer(app);
+let io = require("socket.io")(server);
 let users = new Map();
 
-io.sockets.on('connection', function (socket) {
-    socket.on('connect', () => {
+io.sockets.on("connection", function (socket) {
+  socket.on("connect", () => {});
 
-    });
+  socket.on("register", function (username) {
+    username = username.trim();
+    if (users.has(username)) {
+      socket.emit("conflict username");
+    } else {
+      socket.username = username;
+      users.set(username, []);
+      socket.emit("register success");
+      io.sockets.emit("system", `${username} connected`);
+    }
+  });
 
-    socket.on('register', function (username) {
-        username = username.trim();
-        if (users.has(username)) {
-            socket.emit('conflict username');
-        } else {
-            socket.username = username;
-            users.set(username, []);
-            socket.emit('register success');
-            io.sockets.emit('system', `${username} connected`);
-        }
-    });
+  socket.on("message", function (message) {
+    message = message.trim();
+    if (message === "") return;
+    let username = socket.username;
+    if (username === undefined || username === "") {
+      username = "Anonymous";
+    }
+    io.emit("message", message, username);
+  });
 
-    socket.on('message', function (message) {
-        message = message.trim();
-        if(message === "") return;
-        let username = socket.username;
-        if (username === undefined || username === "") {
-            username = "Anonymous";
-        }
-        io.emit('message', message, username);
-    });
-
-    socket.on('disconnect', () => {
-        if (socket.username) {
-            users.delete(socket.username);
-            io.sockets.emit('system', `${socket.username} connected`);
-        }
-    });
+  socket.on("disconnect", () => {
+    if (socket.username) {
+      users.delete(socket.username);
+      io.sockets.emit("system", `${socket.username} dis connected`);
+    }
+  });
 });
 
+app.use(serveStatic(path.join(__dirname, "public"), { maxAge: "600000" }));
 
-app.use(serveStatic(path.join(__dirname, 'public'), {maxAge: '600000'}));
+app.use(function (req, res) {
+  res.status(404);
+  res.send({ error: "Not found" });
+});
 
 server.listen(process.env.PORT || 3000);
---
+++ file.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const { v4: uuid } = require("uuid");
+
+const upload_path = "./public/uploads";
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, callback) {
+      callback(null, upload_path);
+    },
+    filename: function (req, file, callback) {
+      let extension = file.originalname.split(".").pop();
+      callback(null, uuid() + "." + extension);
+    },
+  }),
+});
+
+router.post("/", upload.single("file"), (req, res, next) => {
+  const { file } = req;
+  res.json({
+    path: "/uploads/" + file.filename,
+  });
+});
+
+module.exports = router;
